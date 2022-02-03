08b5d69144125d998f643375f9e11400b77d3493
S010MON <10490509+S010MON@users.noreply.github.com>
2022-02-03 11:26:10 +0100

    fixes swapped alphas

--- competeSpecies.m
+++ competeSpecies.m
@@ -7,7 +7,7 @@ function [x_prime, y_prime] = competeSpecies(epsilon_1,	sigma_1, alpha_1, x_t,
 % alpha = effect on the opposite species
 % x_t and y_t = population of species x and y at time t respectively
 
-	x_prime = (epsilon_1 - (sigma_1 * x_t) - (alpha_1 * y_t)) * x_t;
-	y_prime = (epsilon_2 - (sigma_2 * y_t) - (alpha_2 * y_t)) * y_t;
+	x_prime = (epsilon_1 - (sigma_1 * x_t) - (alpha_2 * y_t)) * x_t;
+	y_prime = (epsilon_2 - (sigma_2 * y_t) - (alpha_1 * x_t)) * y_t;
 
 end%function

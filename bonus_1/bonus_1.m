72ae881770bd6a7cb17d1cba76be32fa074d2342
S010MON <10490509+S010MON@users.noreply.github.com>
2022-02-20 12:59:58 +0100

    Challenge 2 complete

--- bonus_1/bonus_1.m
+++ bonus_1/bonus_1.m
@@ -12,7 +12,7 @@ c = [x_dot(2,1), y_dot(2,1)]
 % Part (b): 
 syms x y;
 J = jacobian([2*x - 3 * x * y + y^2, x + y + x * y - 5] ,[x,y]);
-f = @(x,y) [2 - 3*y, 2*y - 3*x; y + 1, x + 1]; % This is the result above manually inputted
+f = @(x,y) [2 - 3*y, 2*y - 3*x; y + 1, x + 1]; % This is the result from above manually inputted
 A = f(1,2)
 
 
@@ -22,17 +22,50 @@ A = f(1,2)
 r = 120;
 l = 0.01;
 c = 0.002;
-v_1 = 1;
 
-A = [ (-1/(l*c)), 0;
-     -0, (-r*c/(l*c))];
-B = [1/(l*c);
-     0];
+A = [      0    ,    1    ;
+      (-1/(l*c)), (-r/l) ];
+B = [    0    ;
+      1/(l*c)];
 C = [1, 0];
 D = 0;
 
 RCL_ss = ss(A,B,C,D)
 
-RCL_tf = tf(RCL_ss)
+
+% Part (b): Simulate the state-space system for 10 second
+T = 0:0.001:2;              % only 2 seconds to keep it small
+U = sin(50 * 2 * pi * T);
+Y = lsim(RCL_ss, U, T);
+
+%plot(T, Y);
+
+
+% Part (c): Is this system stable or not?
+% Yes as it tends towards an equilibrium around 0
+
+%% Challenge 3
+% Use the Matlab “residue” command to perform partial fraction expansion on the following
+% discrete-time transfer function
+
+b = [1, 1, 1/4];
+a = [1, 1/4, 0];
+[r,p,k] = residuez(b, a)
+
+
+b = [1, 1, 1/4];
+a = [1, 1/4, 0, 0];
+
+[r, p, k] = residue(b, a)
+
+% The partial fraction expansion found is:
+%            0.5000              0.5000 
+% H(z) = ---------------- + ----------------
+%          z + 0.5000i          z - 0.5000i
+
+
+
+
+
 
 

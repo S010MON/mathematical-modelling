aeee44f8cd578957d529d6f9256ff0cf56962a30
S010MON <leon@bdebnath.com>
2022-03-13 22:41:44 +0100

    bonus 2 first attempt

--- bonus_2/bonus2_2021_2022.m
+++ bonus_2/bonus2_2021_2022.m
@@ -7,36 +7,48 @@
 % out the 60Hz powerline interference and another notch filter to cancel 
 % its 120Hz harmonic. Combine these in a single transfer function
 fs=300;
+t = 0: 1/fs :5;
 
 % first make notch filter for 60Hz powerline
 z1 = [pi * 120i, pi * -120i];
 p1 = [-0.1 + pi * 120i, -0.1 - pi * 120i];
-b1 = [1, 14400 * pi^2];
-a1 = [1, 0.2, 0.1 - pi^2 * 14400.001];
+b1 = [1, 0, 14400 * pi^2];
+a1 = [1, 0.2, 0.1 + 14400 * pi^2];
+sys_1 = tf(b1, a1)
 
 % then make notch filter for 120Hz harmonic
 z2 = [pi * 240i, pi * -240i];
 p2 = [-0.1 + pi * 240i, -0.1 - pi * 240i];
-b2 = [1, 57600 * pi^2];
-a2 = [1, 0.2, 0.1 - pi^2 * 57600.001];
+b2 = [1, 0, 57600 * pi^2];
+a2 = [1, 0.2, 0.1 + 57600 * pi^2];
+sys_2 = tf(b2, a2)
+
+% figure;
+% bode(sys_1)
+% hold on;
+% bode(sys_2)
+% hold off;
 
 % Finally combine the notch filters in a single transfer function
-z_c % zeros of combined transfer function
-p_c % poles of combined transfer function
-b_c % numerator of combined transfer function
-a_c % denominator polynomial of combined transfer function
-sys1 % Transfer function of combined system
+z_c = [z1, z2]
+p_c = [p1, p2]
+% b_c = [1, 0, 72000 * pi^2, 0, 829440000 * pi^4];
+b_c = [1, 0, 710600, 0, 80790000000];
+% a_c = [1, 0.4, 0.2 + pi^2 * 72000i, 0.04 + pi^2 * 14400i, 829440000.01 + pi^2 * 7200i];
+a_c = [1, 0.4, 710600i, 142100i, 80800000000];
+
 
 %% Challenge 1b
 % Plot the magnitude response (bode magnitude plot)
 % first compute the frequency response
-omega = linspace(0,2*pi,1000); % frequencies in radians
-H2 %frequency response
+omega = linspace(0,2*pi,1000);     % frequencies in radians
+signal = @(w) sin(t * w);
+H1 = tf(b_c, a_c)
+
 % Plot the magnitude response (absolute value of the frequency response)
 figure(4);
-% ==> your code here
-xlabel('frequency (Hz)');
-title('Magnitude response Task 2b')
+bode(H1)
+title('Magnitude response Task 1b')
 
 % Make a pole-zero plot
 figure;
@@ -46,50 +58,65 @@ tmpy = imag(exp(omega*1i));
 plot(tmpx,tmpy,'--k');
 % plot real axis
 hold on
-plot(linspace(-1.1,1.1,100),zeros(1,100),'k:');
-% plot imaginary axis
+plot(linspace(-1.1,1.1,100),zeros(1,100),'k:'); % plot imaginary axis
 plot(zeros(1,100),linspace(-1.1,1.1,100),'k:');
-% plot poles with an x
-% plot zeros with an o
+
+zero_real = cos(real(z_c));
+zero_imag = sin(imag(z_c));
+pole_real = cos(real(p_c));
+pole_imag = sin(imag(p_c));
+plot(pole_imag, pole_real,'x');        % plot poles with an x
+plot(zero_imag, zero_real,'o');        % plot zeros with an o
+
 xlabel('real axis')
 ylabel('imaginary axis')
 axis square
 title('Pole-zero plot Challenge 1b')
 
 %% Challenge 1c
+
+data = load("data_MM_bonus2_challenge1.mat");
+u = data.u;
+
 figure;
 subplot(3,1,1)
-% do some plotting
-title ('Time domain inout');
+plot(1:length(u), u)
+title ('Time domain input');
 xlabel('time (s)') % also make sure you actually have seconds that you show 
+
 subplot(3,1,2)
 plot((0:length(u)-1)*(fs/length(u)),abs(fft(u))); % magnitude spectrum
 title('Magnitude spectrum input');
 xlabel('write the meaning and units here')
+
 subplot(3,1,3)
-% todo: add the phase spectum
+U = abs(fft(u));
+plot(omega, U);
 title('Phase spectrum input');
 xlabel('write the meaning and units here')
 ylabel('write the meaning and units here')
 
-%% Challenge 1d
-figure;
-plot((0:length(u)-1)*(fs/length(u)),abs(fft(u))); % magnitude spectrum input
-hold on
-% todo: magnitude spectrum output
-title('Magnitude spectrum');
-xlabel('write the meaning and units here')
 
-%% Challenge 2
-T=0.01; % sampling interval
-A=[-1 2 -3;-2 0 -1;2 1 -1]; % System matrix
-B=[1; 0; -2]; % input matrix
-C=[0 0 2]; % output matrix
-D=0; % direct feedthrough
 
-t=0:T:5-T; %create time vector
-u=linspace(-1,1,length(t))+sin(2*t); % sampled time series
+% %% Challenge 1d
+% figure;
+% plot((0:length(u)-1)*(fs/length(u)),abs(fft(u))); % magnitude spectrum input
+% hold on
+% % todo: magnitude spectrum output
+% title('Magnitude spectrum');
+% xlabel('write the meaning and units here')
+% 
+% %% Challenge 2
+% T=0.01; % sampling interval
+% A=[-1 2 -3;-2 0 -1;2 1 -1]; % System matrix
+% B=[1; 0; -2]; % input matrix
+% C=[0 0 2]; % output matrix
+% D=0; % direct feedthrough
+% 
+% t=0:T:5-T; %create time vector
+% u=linspace(-1,1,length(t))+sin(2*t); % sampled time series
+% 
+% %% Challenge 2a
+% 
+% %% Challenge 2b
 
-%% Challenge 2a
-
-%% Challenge 2b

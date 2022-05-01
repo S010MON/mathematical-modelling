4a6e746202f769c2245387b3625b687f87c84b68
leon <leon@pop-os.localdomain>
2022-05-01 17:09:31 +0200

    box plot added

--- sim_stats/assignment.m
+++ sim_stats/assignment.m
@@ -1,16 +1,23 @@
-%Start of the file
 
 data = load("dataIndSSA2022.mat");
 d1 = data.task1_data;
 
-d1_length = length(d1)
-d1_min = min(data.task1_data)
-d1_max = max(d1)
-d1_mean = mean(d1)
-d1_median = median(d1)
+fprintf('Task 1\n'); 
+fprintf('%d datapoints in the data set\n', length(d1)); 
+fprintf('The minimum value is: %d\n', min(d1)); 
+fprintf('The maximum value is: %d\n', max(d1));
+fprintf('The median value is: %d\n', median(d1)); 
+fprintf('The mean value is: %d\n', mean(d1)); 
 
+% A scatter plot is the easiest and thus first 
+figure(1);
 scatter(1:d1_length, d1);
+title('Task 1: Scatter plot')
 
-hist(d1, 1000)
-
-boxchart(1:d1_length, d1)
+% figure(2);
+% hist(d1, 1000);
+% title('Task 1: Histogram')
+% 
+% figure(3);
+% boxplot(d1)
+% title('Task 1: Box plot')

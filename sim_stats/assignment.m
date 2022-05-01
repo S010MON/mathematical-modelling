a3f2267cdc5a9914e6a37217c83f5ae68546b674
leon <leon@pop-os.localdomain>
2022-05-01 15:41:13 +0200

    2x visualisations

--- sim_stats/assignment.m
+++ sim_stats/assignment.m
@@ -3,7 +3,14 @@
 data = load("dataIndSSA2022.mat");
 d1 = data.task1_data;
 
-min = min(d1)
-max = max(d1)
-mean = mean(d1)
-scatter(data.task1_data, 1:length(d1))
+d1_length = length(d1)
+d1_min = min(data.task1_data)
+d1_max = max(d1)
+d1_mean = mean(d1)
+d1_median = median(d1)
+
+scatter(1:d1_length, d1);
+
+hist(d1, 1000)
+
+boxchart(1:d1_length, d1)

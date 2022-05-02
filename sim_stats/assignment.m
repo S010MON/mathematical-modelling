daf036686dff605b6f63854636b6ba709d96b2c7
S010MON <leon_debnath@hotmail.co.uk>
2022-05-02 16:29:29 +0200

    7 Number summary

--- sim_stats/assignment.m
+++ sim_stats/assignment.m
@@ -2,42 +2,52 @@
 data = load("dataIndSSA2022.mat");
 d1 = data.task1_data;
 
-fprintf('Task 1\n'); 
-fprintf('%d datapoints in the data set\n', length(d1)); 
+%% 7 Numbers 
+fprintf('\nTask 1\n'); 
+n = length(d1);
+fprintf('There are %d datapoints in the data set\n', n); 
 fprintf('The minimum value is: %f\n', min(d1)); 
 fprintf('The maximum value is: %f\n', max(d1));
+fprintf('The mean value is: %f\n\n', mean(d1));
+fprintf('The 2nd percentile is: %f\n', prctile(d1, 2)); 
+fprintf('The 9th percentile is: %f\n', prctile(d1, 9)); 
+fprintf('The 1st quartile is: %f\n', prctile(d1, 25)); 
 fprintf('The median value is: %f\n', median(d1));
-d1_mean = mean(d1);
-fprintf('The mean value is: %f\n', mean(d1)); 
+fprintf('The 3rd quartile is: %f\n', prctile(d1, 75)); 
+fprintf('The 91st percentile is: %f\n', prctile(d1, 91)); 
+fprintf('The 98th percentile is: %f\n', prctile(d1, 98)); 
 
 % A scatter plot is the easiest and thus first visualisation to be conducted
 % the plot shows a strong cluster of values around the mean line, with a
 % large gap between 80 and 100 on the Y axis as well as an unusual
 % formation of points on the 0 line.
-figure(1);
-scatter(1:length(d1), d1);
-hold on;
-plot([0,length(d1)], [d1_mean, d1_mean])
-hold off;
-title('Task 1: Scatter plot')
+d1_mean = mean(d1);
+% figure(1);
+% scatter(1:length(d1), d1);
+% hold on;
+% plot([0,length(d1)], [d1_mean, d1_mean])
+% hold off;
+% title('Task 1: Scatter plot')
 
 % At first glance a histogram shows a poisson distribution, this plot also
 % confirms that the large number of values clustered at 0 do not fit into
 % the curve of the distribution. The tail of the distribution also does not
 % look normal, with an unexpected rise in values around 100
-figure(2);
-hist(d1, 1000);
-title('Task 1: Histogram')
+
+% figure(2);
+% hist(d1, 1000);
+% title('Task 1: Histogram')
 
 % The box and whisker plot confirms the abnormality of the tail falls
 % ouside the interquartile range of the distribution.  We can thus conclude
 % that between 
-figure(3);
-boxplot(d1, 'Notch', 'on')
-title('Task 1: Box plot')
 
-figure(4);
-d1_clean = d1(d1 <= 99);
-d1_clean = d1_clean(d1_clean > 0);
-hist(d1_clean, 1000);
+% figure(3);
+% boxplot(d1, 'Notch', 'on')
+% title('Task 1: Box plot')
+
+% figure(4);
+% d1_clean = d1(d1 <= 99);
+% d1_clean = d1_clean(d1_clean > 0);
+% hist(d1_clean, 1000);
 
